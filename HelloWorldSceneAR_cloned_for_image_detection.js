51a0f7b5780181bf4e2b15a459662772b51fac90
WesleyCKY <wesleychin9966@gmail.com>
2021-07-31 11:08:05 +0800

    first commit

---
+++ HelloWorldSceneAR_cloned_for_image_detection.js
@@ -0,0 +1,90 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {StyleSheet} from 'react-native';
+
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroBox,
+  ViroMaterials,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroAnimations,
+  ViroSurface,
+} from 'react-viro';
+
+export default class HelloWorldSceneAR extends Component {
+
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text : "Initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene>
+      <ViroARImageMarker target={"targetOne"} >
+        <ViroBox position={[0, .25, 0]} scale={[.5, .5, .5]} />
+      </ViroARImageMarker>
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text : "Hello World!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',  
+  },
+});
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grid_bg.jpg'),
+  },
+});
+
+ViroAnimations.registerAnimations({
+  rotate: {
+    properties: {
+      rotateY: "+=90"
+    },
+    duration: 250, //.25 seconds
+  },
+});
+
+ViroARTrackingTargets.createTargets({
+  "targetOne" : {
+    source : require('./res/targetOne.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.1 // real world width in meters
+  },
+});
+
+module.exports = HelloWorldSceneAR;
